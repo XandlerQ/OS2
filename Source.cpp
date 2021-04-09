4a19b36153fbf9d9da2667deba42cb29f8d14ba5
XandlerQ <36069104+XandlerQ@users.noreply.github.com>
2021-04-09 23:44:27 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -26,8 +26,6 @@ int main()
 
 	BiCGSTAB problem("D:\\screens\\Study\\ня\\work2\\opersis2\\1138_bus.txt", "D:\\screens\\Study\\ня\\work2\\opersis2\\b1138.txt", test);
 
-    //std::cout << problem.getA();
-
 	while (threads != 0)
 	{
 		std::cout << "Input thread number >>>";
@@ -42,24 +40,14 @@ int main()
         
 
 		double timeStart = omp_get_wtime();
-		//clock_t start = clock();
 		problem.solve(0.00001);
-		//clock_t end = clock();
 		double timeEnd = omp_get_wtime();
 
 		auto res = problem.getSolution();
 
-		for (int i = 0; i < res.size(); i++)
-		{
-			std::cout << res.at(i) << std::endl;
-		}
+        printvector(res);
 
-        
-
-		//std::cout << std::endl << "/-/-/-/-/-/-/-/" << (double)(end - start) / CLOCKS_PER_SEC << std::endl;
-		
-
-        std:: cout << std::cout << "Precision : " << problem.evaluateSolution();
+        std:: cout << std::endl << "Precision : " << problem.evaluateSolution();
 
         std::cout << std::endl << "				*****             " << timeEnd - timeStart << std::endl;
 
@@ -68,68 +56,3 @@ int main()
 	return 0;
 
 }
-/*
-#include <iostream>
-#include <vector>
-#include <ctime>
-#include "BiCGSTAB.h"
-#include <omp.h>
-
-static bool test;
-
-
-int main()
-{
-    omp_set_dynamic(0);
-    srand(time(NULL));
-
-    //std::cout << "Testing mode? >>>";
-
-    //std::cin >> test;
-
-    //BiCGSTAB problem("C:\Users\Admin\source\repos\oper_sis\oper_sis\A.txt", "C:\Users\Admin\source\repos\oper_sis\oper_sis\b.txt", test);
-
-    while (threads != 0)
-    {
-        std::cout << "Input thread number >>>";
-
-
-        std::cin >> threads;
-
-
-
-        //omp_set_num_threads(threads);
-
-        double timeStart = omp_get_wtime();
-        //clock_t start = clock();
-        //clock_t end = clock();
-
-#pragma omp parallel for num_threads(threads)
-
-        for (int i = 0; i < 99999999; i++)
-            double a = 1. / (rand()%1 + 0.1);
-
-        double timeEnd = omp_get_wtime();
-        std::cout << std::endl << "                             " << timeEnd - timeStart << std::endl;
-
-        //    double timeStart = omp_get_wtime();
-        //    //clock_t start = clock();
-        //    problem.solve(0.001);
-        //    //clock_t end = clock();
-        //    double timeEnd = omp_get_wtime();
-
-        //    auto res = problem.getSolution();
-
-        //    for (int i = 0; i < res.size(); i++)
-        //    {
-        //        std::cout << res.at(i) << std::endl;
-        //    }
-
-        //    std::cout << std::endl << problem.getIterCount();
-
-        //    //std::cout << std::endl << "/-/-/-/-/-/-/-/" << (double)(end - start) / CLOCKS_PER_SEC << std::endl;
-        //    std::cout << std::endl << "                             " << timeEnd - timeStart << std::endl;
-
-    }
-    return 0;
-}*/
